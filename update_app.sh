9d233c7613a64b591cbd9a8b8e40de4e675c3a6c
Marcin Taff <marcin.taff@gmail.com>
2024-05-06 12:21:14 +0200

    hotfix

--- update_app.sh
+++ update_app.sh
@@ -1,13 +1,15 @@
-#!/bin/bash
+#!/bin/bash 
+
+# chmod +x update_app.sh => ./update_app.sh
 
 # Name of your Next.js application
 APP_NAME="softaff website"
 
-# Change to your application directory
-cd /www/portfolio-nextjs
-
 # Stop the application
 pm2 stop "$APP_NAME"
+
+# Remove old process
+pm2 delete "$APP_NAME"
 
 # Pull the latest changes from Git
 git pull
@@ -17,3 +19,9 @@ npm run build
 
 # Start the application again
 pm2 start npm --name "$APP_NAME" -- run start
+
+# save the list of running PM2 processes
+pm2 save
+
+# Generate the startup script
+sudo env PATH=$PATH:/home/ubuntu/n/bin /home/ubuntu/n/lib/node_modules/pm2/bin/pm2 startup systemd -u ubuntu --hp /home/ubuntu
