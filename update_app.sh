f4c54d99c5ba7dcb0f5ddcddb6dba1fc1fd300e8
Marcin Taff <marcin.taff@gmail.com>
2024-05-06 12:29:49 +0200

    Updated comments

--- update_app.sh
+++ update_app.sh
@@ -1,29 +1,31 @@
-#!/bin/bash 
+#!/bin/bash
 
-# chmod +x update_app.sh => ./update_app.sh
+# Make the script executable with `chmod +x update_app.sh` and then execute with `./update_app.sh`.
 
-# Name of your Next.js application
+# Define the name of your Next.js application.
 APP_NAME="softaff website"
 
-# Stop the application
+# Stop the running application using PM2.
 pm2 stop "$APP_NAME"
 
-# Remove old process
+# Remove the old process for the application.
 pm2 delete "$APP_NAME"
 
+# Discard any changes in the Git working directory and reset to the latest commit.
 git reset --hard
 
-# Pull the latest changes from Git
+# Pull the latest changes from the Git repository.
 git pull
 
-# Rebuild the Next.js application
+# Rebuild the Next.js application with the latest changes.
 npm run build
 
-# Start the application again
+# Start the application again using PM2.
 pm2 start npm --name "$APP_NAME" -- run start
 
-# save the list of running PM2 processes
+# Save the list of running PM2 processes for future reference and automatic restart.
 pm2 save
 
-# Generate the startup script
+# Generate a startup script to allow PM2 to restart your application automatically on system boot.
+# pm2 startup
 sudo env PATH=$PATH:/home/ubuntu/n/bin /home/ubuntu/n/lib/node_modules/pm2/bin/pm2 startup systemd -u ubuntu --hp /home/ubuntu
